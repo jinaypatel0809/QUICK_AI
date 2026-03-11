39c9efff18a884be5eaf03b24609da69b2016088
Jinay Patel <jinaypatel0809@gmail.com>
2026-03-11 17:46:16 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import WriteArticle from './pages/WriteArticle'
+import BlogTitles from './pages/BlogTitles'
+import GenerateImages from './pages/GenerateImages'
+import RemoveBackground from './pages/RemoveBackground'
+import RemoveObject from './pages/RemoveObject'
+import ReviewResume from './pages/ReviewResume'
+import Community from './pages/Community'
+import { useAuth } from '@clerk/clerk-react'
+import { useEffect } from 'react'
+
+const App = () => {
+
+  const {getToken} = useAuth()
+  useEffect(()=>{
+    getToken().then((token)=>console.log(token));
+  },[])
+  return (     
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/ai' element={<Layout />}>
+          <Route index element={<Dashboard />}/>
+          <Route path='write-article' element={<WriteArticle/>}></Route>
+          <Route path='blog-titles' element={<BlogTitles/>}></Route>
+          <Route path='generate-images' element={<GenerateImages/>}></Route>
+          <Route path='remove-background' element={<RemoveBackground/>}></Route>
+          <Route path='remove-object' element={<RemoveObject/>}></Route>
+          <Route path='review-resume' element={<ReviewResume/>}></Route>
+          <Route path='community' element={<Community/>}></Route>
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
